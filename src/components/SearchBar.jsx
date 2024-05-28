59ef65d7cb50ad77755ccffc312c135194ca6835
Gearóid O'Ceallacháin <gocealla@gmail.com>
2024-05-28 18:07:32 +0300

    Copied searchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,44 +1,57 @@
-import React, { useState, useEffect } from 'react'
-import { FaSearch } from 'react-icons/fa'
-import "./SearchBar.css"
-import { getSpotifyToken, searchSpotify } from './spotify'
+import React, { useState, useEffect, useCallback } from 'react';
+import { FaSearch } from 'react-icons/fa';
+import "./SearchBar.css";
+import { getSpotifyToken, searchSpotify } from './spotify';
 
 export const SearchBar = ({ setResults }) => {
-    const [input, setInput] = useState("")
-    const [token, setToken] = useState("")
+    const [input, setInput] = useState("");
+    const [token, setToken] = useState("");
 
     useEffect(() => {
         const fetchToken = async () => {
-            const token = await getSpotifyToken();
-            setToken(token);
+            try {
+                const token = await getSpotifyToken();
+                setToken(token);
+            } catch (error) {
+                console.error("Error fetching Spotify token:", error);
+            }
         };
         fetchToken();
     }, []);
 
-
-    const fetchData = async (value) => {
+    const fetchData = useCallback(async (value) => {
         if (token) {
-            const results = await searchSpotify(token, value);
-            setResults(results)
-        } 
-    }
+            try {
+                const results = await searchSpotify(token, value);
+                setResults(results);
+            } catch (error) {
+                console.error("Error fetching data from Spotify:", error);
+            }
+        }
+    }, [token, setResults]);
 
     const handleChange = (value) => {
         setInput(value);
         if (value) {
-            fetchData(value)
-        } 
-    }
-//https://www.youtube.com/watch?v=sWVgMcz8Q44
+            debounceFetchData(value);
+        }
+    };
 
-  return (
-    <div className='input-wrapper'>
-        <FaSearch id="search-icon" />
-        <input
-         placeholder='Type a song.'
-         value ={input} 
-         onChange={(e) => handleChange(e.target.value)}
-         />
-    </div>
-  )
-}
+    const debounceFetchData = useCallback(
+        debounce((value) => {
+            fetchData(value);
+        }, 500),
+        [fetchData]
+    );
+
+    return (
+        <div className='input-wrapper'>
+            <FaSearch id="search-icon" />
+            <input
+                placeholder='Type artist, song or album.'
+                value={input} 
+                onChange={(e) => handleChange(e.target.value)}
+            />
+        </div>
+    );
+};
