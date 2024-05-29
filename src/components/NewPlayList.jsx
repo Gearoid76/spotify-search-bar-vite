d48168389d11345223d638376733e08d92030764
Gearóid O'Ceallacháin <gocealla@gmail.com>
2024-05-29 19:25:33 +0300

    created NewPlaylist and css

--- src/components/NewPlayList.jsx
+++ src/components/NewPlayList.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import "./SearchResultsList.css"
+import './NewPlayList.css';
 
-export const NewPlayList = ({ playlist }) => {
-  return (
-    <div className='new-playlist'> 
-        <h2>New Playlist</h2>
-        {playlist.map((song, index) => (
-            <div key={index} className="results-list">
-              <div className="playlist-song">{song.song}</div>
-              <div className="playlist-artist">{song.artist}</div>
-              <div className="playlist-album">{song.album}</div>
-            </div>
-        ))}
+export const NewPlayList = ({ playlist, playlistName, handlePlaylistNameChange }) => {
+    return (
+        <div className='new-playlist'>
+            <h2>New Playlist</h2>
+            <input
+            type="text"
+            placeholder="Enter playlist name"
+            value={playlistName}
+            onChange={handlePlaylistNameChange}
+            className="playlist-name-input"
+            />
+            {playlist.map((song, index) => (
+                <div key={index} className="playlist-item">
+                    <div className="playlist-song">{song.song}</div>
+                    <div className="playlist-artist">{song.artist}</div>
+                    <div className="playlist-album">{song.album}</div>
+                </div>
+            ))}
     </div>
   );
 };
-
-   
-
-             
-             
-    
 
