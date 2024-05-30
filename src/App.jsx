74d6fc7934281874f3ea8ce03e773a0bdc254b62
Gearóid O'Ceallacháin <gocealla@gmail.com>
2024-05-30 22:21:11 +0300

    made updates to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,64 @@
-import React,{ useState } from 'react'
+import React, { useState, useEffect } from 'react';
 import { redirectToAuthCodeFlow, getAccessToken } from './authentication';
-import './App.css'
+import { createSpotifyPlaylist } from './components/createSpotifyPlaylist'; // may have problems
+import { addTracksToPlaylist } from './components/addTracksToPlaylist';      // may have problems
+import './App.css';
 import { SearchBar } from './components/SearchBar'
 import { SearchResultsList } from './components/SearchResultsList';
 import { NewPlayList } from './components/NewPlayList';
 
 const clientId = import.meta.env.VITE_CLIENT_ID;
+//const clientSecret = import.meta.env.ITE_CLIENT_SECRET;
 const params = new URLSearchParams(window.location.search);
 const code = params.get("code");
-
-if (!code) {
-  redirectToAuthCodeFlow(clientId);
-} else {
-  (async () => {
-    try {
-      const accessToken = await getAccessToken(clientId, code);
-    } catch (error) {
-      console.error('Error', error.message);
-    }
-  })();
-}
+let accessToken = null;
 
 function App() {
   const [results, setResults] = useState([]);
   const [playlist, setPlaylist] = useState([]);
+  const [accessToken, setAccessToken] = useState(null);
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const code = params.get("code");
+
+    if (!code) {
+      redirectToAuthCodeFlow(clientId);
+    } else {
+      (async () => {
+        try {
+          const token = await getAccessToken(clientId, code);
+          setAccessToken(token);
+        } catch (error) {
+          console.error('Error', error.message);
+        }
+      })();
+  }
+  }, []);
 
   const addToPlayList = (result) => {
     setPlaylist((prevPlaylist) => [...prevPlaylist, result]);
   };
 
+  const savePlaylist = async (name, playlist) => {
+    try {
+      const playlistId = await createSpotifyPlaylist(name, accessToken);
+      const trackUris = playlist.map(song => song.uri);
+      await addTracksToPlaylist(playlistId, trackUris, accessToken);
+      alert('Playlist saved to spotify');
+    } catch (error) {
+      console.error('Error saving playlist:', error);
+      alert('Failed to save to playlist');
+    }
+  };
+
   return (
    <div className='App'>
       <div className="search-bar-container">
-          <SearchBar setResults={setResults}/>
+          <SearchBar setResults={setResults} />
           <div className='resultAndPlaylistColumn'>
-          <SearchResultsList results={results} addToPlayList={addToPlayList} />
-          <NewPlayList playlist={playlist} />
+            <SearchResultsList results={results} addToPlayList={addToPlayList} />
+            <NewPlayList playlist={playlist} savePlaylist={savePlaylist} />
           </div>
       </div>   
    </div>
