39028b00daa7027eabb7aa50416f95f05a6eff65
Gearóid O'Ceallacháin <gocealla@gmail.com>
2024-05-30 22:20:55 +0300

    Created NewPlaylist.jsx

--- src/components/NewPlayList.jsx
+++ src/components/NewPlayList.jsx
@@ -1,14 +1,26 @@
-import React from 'react';
-//import { SearchResultsList } from './SearchResultsList';
+import React, { useState } from 'react';
 import './NewPlayList.css';
-    
-    export const NewPlayList = ({ playlist }) => {
+
+export const NewPlayList = ({ playlist,savePlaylist }) => {
+  const [playlistName, setPlaylistName] = useState('');
+
+  const handleChange = (e) => {
+    setPlaylistName(e.target.value);
+  };
+
+  const handleSave = () => {
+    savePlaylist(playlistName, playlist);
+  };
+
       return (
         <div className='new-playlist'>
           <input 
-            type='text' 
-            
+            type='text'
+            placeholder='Playlist Name'
+            value={playlistName}
+            onChange={handleChange}
           />
+          <button onClick={handleSave}>Save to Spotify</button>
           <div className='playlist'>
             {playlist.map((song, index) => (
               <div key={index} className='playlist-item'>
@@ -19,5 +31,4 @@ import './NewPlayList.css';
         </div>
       );
     };
-    
 
