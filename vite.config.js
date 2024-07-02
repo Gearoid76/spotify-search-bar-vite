43e30ebaa76b7994f769ebe9caeb748c21419446
Gearóid O'Ceallacháin <gocealla@gmail.com>
2024-07-02 15:18:52 +0300

    server: {     open: 'docs/index.html',   },

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ dns.setDefaultResultOrder('verbatim')
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    open: 'docs/index.html',
+  },
   plugins: [react()],
   base: '/spotify-search-bar-vite/'
 })
