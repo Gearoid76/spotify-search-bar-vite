240b62580538c72ba0db3d56d1d1a4e50db37313
Gearóid O'Ceallacháin <gocealla@gmail.com>
2024-07-04 12:20:26 +0300

    removed server and open

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  server: {
-    open: true,
-  },
+ 
   base: '/spotify-search-bar-vite/',
   plugins: [react()],
   
