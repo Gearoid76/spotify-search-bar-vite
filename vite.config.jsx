4bc1fd9a6ebe50d6a03aad8fbb67089becdc46e8
Gearóid O'Ceallacháin <gocealla@gmail.com>
2024-07-07 18:27:43 +0300

    added rollupOptions in vite.config.jsx

--- vite.config.jsx
+++ vite.config.jsx
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/spotify-search-bar-vite/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 });
