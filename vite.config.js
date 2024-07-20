76a684c06c9facf087f096c7e8879a26f9203e4f
Gearóid O'Ceallacháin <gocealla@gmail.com>
2024-07-20 11:26:44 +0300

    changed vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+//import { defineConfig } from 'vite'
+//import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+//export default defineConfig({
+//  plugins: [react()],
+//  base: '/spotify-search-bar-vite/'
+//})
+
+import { defineConfig } from "vite";
+import dns from 'node:dns'
+
+dns.setDefaultResultOrder('verbatim')
 export default defineConfig({
-  plugins: [react()],
-  base: '/spotify-search-bar-vite/'
+  
 })
