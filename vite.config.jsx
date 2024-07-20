b006eef87bee0aa208803d5051a8a32b7da2553f
Gearóid O'Ceallacháin <gocealla@gmail.com>
2024-07-20 19:25:53 +0300

    removed 'node_modules'

--- vite.config.jsx
+++ vite.config.jsx
@@ -6,11 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: [
-          /node_modules/
-      ] //,
-     // output: {  
-     //   manualChunks: undefined,
-      },
-    },
+      external: [ ]
+      }
+    }
 });
