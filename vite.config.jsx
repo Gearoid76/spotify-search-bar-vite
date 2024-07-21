47db62b540fa51201adc48bc30896f09d8e51257
Gearóid O'Ceallacháin <gocealla@gmail.com>
2024-07-21 18:26:39 +0300

    went back to '/' to test

--- vite.config.jsx
+++ vite.config.jsx
@@ -1,18 +1,19 @@
 import { defineConfig } from 'vite';
-//import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-//  base: '/spotify-search-bar-vite/', 
-//  build: {
-//    rollupOptions: {
-//      external: []
-//    }
-//  }
-//});
-publicDir: '/spotify-search-bar-vite/',
-build: {
-  outDir: 'dist',
-},
+base: '/', // /spotify-search-bar-vite/
+plugins: [react()], 
+  build: {
+    rollupOptions: {
+      external: []
+    }
+  }
 });
+//publicDir: '/spotify-search-bar-vite/',
+//build: {
+//  outDir: '/dist/',
+//},
+//});
 
 
