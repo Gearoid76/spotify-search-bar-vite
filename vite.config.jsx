87e5f8419053505aee7fdbd432eb277b0e33bfae
Gearóid O'Ceallacháin <gocealla@gmail.com>
2024-07-21 13:17:26 +0300

    changed vite.config removing rollupOptions

--- vite.config.jsx
+++ vite.config.jsx
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+//import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/spotify-search-bar-vite/', 
-  build: {
-    rollupOptions: {
-      external: []
-    }
-  }
+//  base: '/spotify-search-bar-vite/', 
+//  build: {
+//    rollupOptions: {
+//      external: []
+//    }
+//  }
+//});
+publicDir: '/spotify-search-bar-vite/',
+build: {
+  outDir: 'dist',
+},
 });
+
+
